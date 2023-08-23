aa012adee646e07ea91019e6d60e1ac871ab0ca8
Shibasaki-Suzuka <nekonosiawase25@gmail.com>
2023-08-23 23:17:09 +0900

    forEachで表示

--- app.js
+++ app.js
@@ -6,9 +6,18 @@ const app = express();
 app.use(express.static('public'));
 
 // ルーティング
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+  res.render('hello.ejs');
+});
+
 app.get('/top', (req, res) => {
   res.render('top.ejs');
 });
 
+app.get('/index', (req, res) => {
+  res.render('index.ejs');
+});
 
 app.listen(3000);
